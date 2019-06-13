e84eba3c75fc4f9a8d405ed5b981c4f7e919911f
YaoYongtao <35090369+YaoYongtao@users.noreply.github.com>
2019-06-13 15:31:14 -0400

    Update activity.go

--- AgeAnalysis/activity.go
+++ AgeAnalysis/activity.go
@@ -8,7 +8,7 @@ import (
 	"io"
 	"log"
 	"os"
-	"reflect"
+// 	"reflect"
 
 	tf "github.com/tensorflow/tensorflow/tensorflow/go"
 	"github.com/tensorflow/tensorflow/tensorflow/go/op"
@@ -89,7 +89,7 @@ func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
 
 	if preds, ok := result[0].Value().([][]float32); ok {
 		fmt.Println(preds[0])
-		// fmt.Println(reflect.TypeOf(preds[0]))
+// 		fmt.Println(reflect.TypeOf(preds[0]))
 		maxValueIndex = indexOfMax(preds[0])
 		age = ageStage[maxValueIndex]
 		fmt.Println("Age: %s", age)
@@ -97,7 +97,7 @@ func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
 	// *******************************
 
 	ctx.Logger().Debugf("Input serial: %s", input.Serial)
-	ctx.Logger().Debugf("Age: %s", age)
+// 	ctx.Logger().Debugf("Age: %s", age)
 
 
 	return true, nil
