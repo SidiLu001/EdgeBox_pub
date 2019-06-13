7fd96af299ce1479d7b5ee9b9e6a86cf5e6ec44a
YaoYongtao <35090369+YaoYongtao@users.noreply.github.com>
2019-06-13 15:28:13 -0400

    Update activity.go

--- AgeAnalysis/activity.go
+++ AgeAnalysis/activity.go
@@ -88,7 +88,7 @@ func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
 	}
 
 	if preds, ok := result[0].Value().([][]float32); ok {
-		// fmt.Println(preds[0])
+		fmt.Println(preds[0])
 		// fmt.Println(reflect.TypeOf(preds[0]))
 		maxValueIndex = indexOfMax(preds[0])
 		age = ageStage[maxValueIndex]
@@ -97,7 +97,7 @@ func (a *Activity) Eval(ctx activity.Context) (done bool, err error) {
 	// *******************************
 
 	ctx.Logger().Debugf("Input serial: %s", input.Serial)
-	// ctx.Logger().Debugf("Age: %s", age)
+	ctx.Logger().Debugf("Age: %s", age)
 
 
 	return true, nil
